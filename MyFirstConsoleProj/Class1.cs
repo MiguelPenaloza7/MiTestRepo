2edacb3f442c5a48366889b9089a5d6c04b089d9
MiguelPenaloza7 <miguel.penaloza@nice.com>
2022-12-20 17:59:46 -0400

    Add a Class1

---
+++ MyFirstConsoleProj/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleProj
+{
+    internal class Class1
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello Class");
+        }
+    }
+}
