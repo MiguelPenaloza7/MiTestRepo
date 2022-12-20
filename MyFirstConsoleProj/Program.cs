02660a85bf76cdcc0792c3328ea0416f5c9d26bc
MiguelPenaloza7 <miguel.penaloza@nice.com>
2022-12-20 18:05:25 -0400

    Fix bug

--- MyFirstConsoleProj/Program.cs
+++ MyFirstConsoleProj/Program.cs
@@ -2,8 +2,8 @@
 using MyFirstConsoleProj;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola Mundo!");
+Console.WriteLine("Chau Mundo!");
 Console.WriteLine("Hello, World!");
 
 Class1 c1 = new Class1();
